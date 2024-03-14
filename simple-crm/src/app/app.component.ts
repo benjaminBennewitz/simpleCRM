3b777771fbd005b14debb65c2f6d700cf7130e9e
Werbung06 <Werbung06@skuvgele.local>
2024-03-14 17:13:00 +0100

    adding dialog, add new user

--- simple-crm/src/app/app.component.ts
+++ simple-crm/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterModule, MatToolbarModule, MatSidenavModule, MatIconModule],
+  imports: [CommonModule, RouterOutlet, RouterModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- simple-crm/src/app/user/user.component.ts
+++ simple-crm/src/app/user/user.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
+
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatButtonModule, MatTooltipModule, MatDialogModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+
+  constructor(public dialog: MatDialog){
+
+  }
+
+  openDialog(){
+    this.dialog.open(DialogAddUserComponent);
+  }
 
 }
